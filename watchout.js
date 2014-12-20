95d40eabb294ed1422d278a28ff62ef24ea46f86
tmartin1 <timothy.m.martin@gmail.com>
2014-12-19 21:20:18 -0800

    Adds a poorly implemented collision detection.

--- watchout.js
+++ watchout.js
@@ -16,12 +16,51 @@ var drag = d3.behavior.drag()
     });
 ;
 
+//detect player-asteroid collision
+
+//write collision detection function
+////when player.cx to cx+20 or cy to cy+20
+////equals any asteroid x to x+50 or y to y+50
+//console.log("ouch");
+//
+//.call that funciton onto player
+setInterval(function(){
+  if (player.attr("cx") + 25 <= asteroids.each(d.attr("x"))
+  && player.attr("cx") - 25 >= asteroids.each(d.attr("x"))
+  && player.attr("cy") + 25 <= asteroids.each(d.attr("y"))
+  && player.attr("cy") - 25 >= asteroids.each(d.attr("y")) ) {
+    console.dir(asteroids);
+  }
+  console.dir(asteroids.each(function(d){return d.attr("x")}));
+},2000)
+
+
+var smash = function(){
+  var pcx = d3.select(".player").attr("cx");
+  var pcy = d3.select(".player").attr("cy");
+
+}
+
 // create player
 var player = d3.select("g").append("circle")
+    .attr("class","player")
     .attr("r",20)
     .attr("cx", width/2)
-    .attr("cy", height/2)
-    .call(drag);
+    .attr("cy", height/2);
+
+//Add drag behavior to player
+    player.call(drag);
+
+    player.on("tick",function(){
+      if ( d3.select(this).attr("cx") < d3.selectAll(".asteroid").attr("x") + 50
+        && d3.select(this).attr("cx") > d3.selectAll(".asteroid").attr("x")
+        && d3.select(this).attr("cy") < d3.selectAll(".asteroid").attr("y") + 50
+        && d3.select(this).attr("cy") > d3.selectAll(".asteroid").attr("y")
+        ) {
+        console.log("impact!");
+      }
+      console.log("ticking!");
+    });
 
 // populate asteroids
 var asteroidMaker = function(num) {
@@ -65,6 +104,8 @@ setInterval(function() {
   }
   update(data);
 }, 1000);
+
+var asteroids = d3.selectAll(".asteroid");
 
 
 // determine collision
